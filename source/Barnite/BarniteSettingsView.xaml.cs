f750b2f4dc204929c9eabd39f2f2cc06eeab1508
Jeshibu <jeshibu@gmail.com>
2022-05-24 18:17:18 +0200

    Barnite: added settings text suggesting Barcode to PC

--- source/Barnite/BarniteSettingsView.xaml.cs
+++ source/Barnite/BarniteSettingsView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,6 +21,14 @@ namespace Barnite
         public BarniteSettingsView()
         {
             InitializeComponent();
+        }
+
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            // for .NET Core you need to add UseShellExecute = true
+            // see https://docs.microsoft.com/dotnet/api/system.diagnostics.processstartinfo.useshellexecute#property-value
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+            e.Handled = true;
         }
     }
 }
