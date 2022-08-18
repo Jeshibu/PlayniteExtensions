41e9f7f064cc6f9bda6a6f209b70d8cc3a38672b
Jeshibu <jeshibu@gmail.com>
2022-08-19 01:13:17 +0200

    Added Games Size Calculator from darklinkpower's repository

---
+++ source/GamesSizeCalculator/Common/Steam/SteamWeb.cs
@@ -0,0 +1,195 @@
+﻿using AngleSharp.Parser.Html;
+using Playnite.SDK;
+using Playnite.SDK.Data;
+using PluginsCommon.Web;
+using SteamCommon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace SteamCommon
+{
+    class SteamWeb
+    {
+        private static ILogger logger = LogManager.GetLogger();
+        private const string steamGameSearchUrl = @"https://store.steampowered.com/search/?term={0}&ignore_preferences=1&category1=998";
+
+        public static List<GenericItemOption> GetSteamSearchGenericItemOptions(string searchTerm)
+        {
+            return GetSteamSearchResults(searchTerm).Select(x => new GenericItemOption(x.Name, x.GameId)).ToList();
+        }
+
+        public static string GetSteamIdFromSearch(string searchTerm)
+        {
+            var normalizedName = searchTerm.NormalizeGameName();
+            var results = GetSteamSearchResults(normalizedName);
+            results.ForEach(a => a.Name = a.Name.NormalizeGameName());
+
+            var matchingGameName = normalizedName.GetMatchModifiedName();
+            var exactMatch = results.FirstOrDefault(x => x.Name.GetMatchModifiedName() == matchingGameName);
+            if (exactMatch != null)
+            {
+                logger.Info($"Found steam id for search {searchTerm} via steam search, Id: {exactMatch.GameId}");
+                return exactMatch.GameId;
+            }
+
+            logger.Info($"Steam id for search {searchTerm} not found");
+            return null;
+        }
+
+        public static List<StoreSearchResult> GetSteamSearchResults(string searchTerm, string steamApiCountry = null)
+        {
+            var results = new List<StoreSearchResult>();
+            var searchPageSrc = HttpDownloader.DownloadStringAsync(GetStoreSearchUrl(searchTerm, steamApiCountry)).GetAwaiter().GetResult();
+            if (!string.IsNullOrEmpty(searchPageSrc))
+            {
+                var parser = new HtmlParser();
+                var searchPage = parser.Parse(searchPageSrc);
+                foreach (var gameElem in searchPage.QuerySelectorAll(".search_result_row"))
+                {
+                    if (gameElem.HasAttribute("data-ds-packageid"))
+                    {
+                        continue;
+                    }
+
+                    // Game Data
+                    var title = gameElem.QuerySelector(".title").InnerHtml;
+                    var releaseDate = gameElem.QuerySelector(".search_released").InnerHtml;
+                    var gameId = gameElem.GetAttribute("data-ds-appid");
+
+                    // Prices Data
+                    var priceData = gameElem.QuerySelector(".search_price_discount_combined");
+                    var discountPercentage = GetSteamSearchDiscount(priceData);
+                    var priceFinal = GetSteamSearchFinalPrice(priceData);
+                    var priceOriginal = GetSearchOriginalPrice(priceFinal, discountPercentage);
+                    var isDiscounted = priceFinal != priceOriginal && priceOriginal != 0;
+                    GetCurrencyFromSearchPriceDiv(gameElem.QuerySelector(".search_price"), out var currency, out var isReleased, out var isFree);
+
+                    //Urls
+                    var storeUrl = gameElem.GetAttribute("href");
+                    var capsuleUrl = gameElem.QuerySelector(".search_capsule").Children[0].GetAttribute("src");
+
+                    results.Add(new StoreSearchResult
+                    {
+                        Name = HttpUtility.HtmlDecode(title),
+                        Description = HttpUtility.HtmlDecode(releaseDate),
+                        GameId = gameId,
+                        PriceOriginal = priceOriginal,
+                        PriceFinal = priceFinal,
+                        IsDiscounted = isDiscounted,
+                        DiscountPercentage = discountPercentage,
+                        StoreUrl = storeUrl,
+                        IsFree = isFree,
+                        IsReleased = isReleased,
+                        Currency = currency,
+                        BannerImageUrl = capsuleUrl
+                    });
+                }
+            }
+
+            logger.Debug($"Obtained {results.Count} games from Steam search term {searchTerm}");
+            return results;
+        }
+
+        private static void GetCurrencyFromSearchPriceDiv(AngleSharp.Dom.IElement element, out string currency, out bool isReleased, out bool isFree)
+        {
+            if (element.ChildElementCount == 2)
+            {
+                // Discounted Item
+                isReleased = true;
+                currency = GetCurrencyFromPriceString(element.Children[0].Children[0].InnerHtml);
+                isFree = currency == null;
+            }
+            else if (!element.InnerHtml.IsNullOrWhiteSpace())
+            {
+                // Non discounted item
+                isReleased = true;
+                currency = GetCurrencyFromPriceString(element.InnerHtml);
+                isFree = currency == null;
+            }
+            else
+            {
+                // Unreleased
+                isReleased = false;
+                currency = null;
+                isFree = false;
+            }
+
+            return;
+        }
+
+        private static string GetCurrencyFromPriceString(string priceString)
+        {
+            if (!Regex.IsMatch(priceString, @"\d"))
+            {
+                // Game is free
+                return null;
+            }
+
+            return Regex.Match(priceString, @"[^\s]+").Value;
+        }
+
+        private static string GetStoreSearchUrl(string searchTerm, string steamApiCountry)
+        {
+            var searchUrl = string.Format(steamGameSearchUrl, searchTerm);
+            if (!steamApiCountry.IsNullOrEmpty())
+            {
+                searchUrl += $"&cc={steamApiCountry}";
+            }
+
+            return searchUrl;
+        }
+
+        private static double GetSearchOriginalPrice(double priceFinal, int discountPercentage)
+        {
+            if (discountPercentage == 0)
+            {
+                return priceFinal;
+            }
+
+            return (100 * priceFinal) / (100 - discountPercentage);
+        }
+
+        private static int GetSteamSearchDiscount(AngleSharp.Dom.IElement priceData)
+        {
+            var searchDiscountQuery = priceData.QuerySelector(".search_discount");
+            if (searchDiscountQuery.ChildElementCount == 1)
+            {
+                //TODO Improve parsing
+                return int.Parse(searchDiscountQuery.Children[0].TextContent.Replace("-", "").Replace("%", "").Trim());
+            }
+
+            return 0;
+        }
+
+        private static double GetSteamSearchFinalPrice(AngleSharp.Dom.IElement priceData)
+        {
+            return int.Parse(priceData.GetAttribute("data-price-final")) * 0.01;
+        }
+
+        private const string steamAppDetailsMask = @"https://store.steampowered.com/api/appdetails?appids={0}";
+        public static SteamAppDetails GetSteamAppDetails(string steamId)
+        {
+            var url = string.Format(steamAppDetailsMask, steamId);
+            var downloadedString = HttpDownloader.DownloadStringAsync(url).GetAwaiter().GetResult();
+            if (!string.IsNullOrEmpty(downloadedString))
+            {
+                var parsedData = Serialization.FromJson<Dictionary<string, SteamAppDetails>>(downloadedString);
+                if (parsedData.Keys?.Any() == true)
+                {
+                    var response = parsedData[parsedData.Keys.First()];
+                    if (response.success == true && response.data != null)
+                    {
+                        return response;
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ source/GamesSizeCalculator/GamesSizeCalculatorSettings.cs
@@ -0,0 +1,156 @@
+﻿using Newtonsoft.Json;
+using Playnite.SDK;
+using Playnite.SDK.Data;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamesSizeCalculator
+{
+    public class GamesSizeCalculatorSettings : ObservableObject
+    {
+        public DateTime LastRefreshOnLibUpdate = DateTime.Now;
+
+        private bool calculateNewGamesOnLibraryUpdate = true;
+        public bool CalculateNewGamesOnLibraryUpdate { get => calculateNewGamesOnLibraryUpdate; set => SetValue(ref calculateNewGamesOnLibraryUpdate, value); }
+
+        private bool calculateModifiedGamesOnLibraryUpdate = true;
+        public bool CalculateModifiedGamesOnLibraryUpdate { get => calculateModifiedGamesOnLibraryUpdate; set => SetValue(ref calculateModifiedGamesOnLibraryUpdate, value); }
+        private bool getUninstalledGameSizeFromSteam = true;
+        public bool GetUninstalledGameSizeFromSteam { get => getUninstalledGameSizeFromSteam; set => SetValue(ref getUninstalledGameSizeFromSteam, value); }
+        private bool getSizeFromSteamNonSteamGames = true;
+        public bool GetSizeFromSteamNonSteamGames { get => getSizeFromSteamNonSteamGames; set => SetValue(ref getSizeFromSteamNonSteamGames, value); }
+        private bool includeDlcInSteamCalculation = false;
+        public bool IncludeDlcInSteamCalculation { get => includeDlcInSteamCalculation; set => SetValue(ref includeDlcInSteamCalculation, value); }
+        private bool includeOptionalInSteamCalculation = false;
+        public bool IncludeOptionalInSteamCalculation { get => includeOptionalInSteamCalculation; set => SetValue(ref includeOptionalInSteamCalculation, value); }
+
+        private bool getUninstalledGameSizeFromGog = true;
+        public bool GetUninstalledGameSizeFromGog { get => getUninstalledGameSizeFromGog; set => SetValue(ref getUninstalledGameSizeFromGog, value); }
+
+        private bool getSizeFromGogNonGogGames = true;
+        public bool GetSizeFromGogNonGogGames { get => getSizeFromGogNonGogGames; set => SetValue(ref getSizeFromGogNonGogGames, value); }
+
+        private ObservableCollection<string> depotRegionWords = new ObservableCollection<string> { "eu", "europe", "row", "en", "english", "ww" };
+
+        [JsonProperty(ObjectCreationHandling = ObjectCreationHandling.Replace)]
+        public ObservableCollection<string> DepotRegionWords { get => depotRegionWords; set => SetValue(ref depotRegionWords, DeduplicateStrings(value)); }
+
+        private ObservableCollection<string> depotRegionWordsBlacklist = new ObservableCollection<string>
+        {
+            "asia",
+            "aus",
+            "australia",
+            "nz",
+            "usa",
+            "us",
+            "ru",
+            "russia",
+            "germany",
+            "deutschland",
+            "de",
+            "es",
+            "sa",
+            "cn",
+            "china",
+            "chinese",
+            "schinese",
+            "tchinese",
+            "jp",
+            "japanese",
+            "fr",
+            "french"
+        };
+
+        [JsonProperty(ObjectCreationHandling = ObjectCreationHandling.Replace)]
+        public ObservableCollection<string> DepotRegionWordsBlacklist { get => depotRegionWordsBlacklist; set => SetValue(ref depotRegionWordsBlacklist, DeduplicateStrings(value)); }
+
+        private static ObservableCollection<string> DeduplicateStrings(IEnumerable<string> strings)
+        {
+            var output = new ObservableCollection<string>(
+                strings.Select(s => s.Trim().ToLowerInvariant())
+                .Where(s => !string.IsNullOrWhiteSpace(s))
+                .Distinct());
+            return output;
+        }
+    }
+
+    public class GamesSizeCalculatorSettingsViewModel : ObservableObject, ISettings
+    {
+        private readonly GamesSizeCalculator plugin;
+        private GamesSizeCalculatorSettings editingClone { get; set; }
+
+        private GamesSizeCalculatorSettings settings;
+        public GamesSizeCalculatorSettings Settings
+        {
+            get => settings;
+            set
+            {
+                settings = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string RegionWordsString
+        {
+            get => string.Join(Environment.NewLine, settings.DepotRegionWords);
+            set => settings.DepotRegionWords = new ObservableCollection<string>(value.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries));
+        }
+        public string RegionWordsBlacklistString
+        {
+            get => string.Join(Environment.NewLine, settings.DepotRegionWordsBlacklist);
+            set => settings.DepotRegionWordsBlacklist = new ObservableCollection<string>(value.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries));
+        }
+
+        public GamesSizeCalculatorSettingsViewModel(GamesSizeCalculator plugin)
+        {
+            // Injecting your plugin instance is required for Save/Load method because Playnite saves data to a location based on what plugin requested the operation.
+            this.plugin = plugin;
+
+            // Load saved settings.
+            var savedSettings = plugin.LoadPluginSettings<GamesSizeCalculatorSettings>();
+
+            // LoadPluginSettings returns null if not saved data is available.
+            if (savedSettings != null)
+            {
+                Settings = savedSettings;
+            }
+            else
+            {
+                Settings = new GamesSizeCalculatorSettings();
+            }
+        }
+
+        public void BeginEdit()
+        {
+            // Code executed when settings view is opened and user starts editing values.
+            editingClone = Serialization.GetClone(Settings);
+        }
+
+        public void CancelEdit()
+        {
+            // Code executed when user decides to cancel any changes made since BeginEdit was called.
+            // This method should revert any changes made to Option1 and Option2.
+            Settings = editingClone;
+        }
+
+        public void EndEdit()
+        {
+            // Code executed when user decides to confirm changes made since BeginEdit was called.
+            // This method should save settings made to Option1 and Option2.
+            plugin.SavePluginSettings(Settings);
+        }
+
+        public bool VerifySettings(out List<string> errors)
+        {
+            // Code execute when user decides to confirm changes made since BeginEdit was called.
+            // Executed before EndEdit is called and EndEdit is not called if false is returned.
+            // List of errors is presented to user if verification fails.
+            errors = new List<string>();
+            return true;
+        }
+    }
+}
