78224f59af681ec03ceb2460589f09deb8f092bb
SparrowBrain <qwxmail@gmail.com>
2022-11-24 23:10:47 +0200

    Viveport Library: Fixed failure to parse malformed installed_apps.json

---
+++ source/ViveportLibrary.Tests/AppDataReaderTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using Xunit;
+
+namespace ViveportLibrary.Tests
+{
+    public class AppDataReaderTests
+    {
+        [Fact]
+        public void GetInstalledApps_ReturnsInstalledApps()
+        {
+            var appDataReader = new AppDataReader("installed_apps.json");
+
+            var installedApps = appDataReader.GetInstalledApps();
+
+            Assert.Equal(2, installedApps.Count());
+        }
+
+        [Fact]
+        public void GetInstalledApps_IgnoresMalformedEntries_WhenMalformedEntriesExist()
+        {
+            var appDataReader = new AppDataReader("installed_apps_malformed.json");
+
+            var installedApps = appDataReader.GetInstalledApps();
+
+            Assert.Equal(2, installedApps.Count());
+            Assert.DoesNotContain(installedApps, x => x.Title == "Malformed Game");
+        }
+
+        [Fact]
+        public void GetInstalledApps_ReturnsNull_WhenNoFileExists()
+        {
+            var appDataReader = new AppDataReader("non_existing_file.json");
+
+            var installedApps = appDataReader.GetInstalledApps();
+
+            Assert.Null(installedApps);
+        }
+
+        [Fact]
+        public void GetInstalledApps_ReturnsNull_WhenFileIsEmpty()
+        {
+            var appDataReader = new AppDataReader("empty.json");
+
+            var installedApps = appDataReader.GetInstalledApps();
+
+            Assert.Null(installedApps);
+        }
+    }
+}
