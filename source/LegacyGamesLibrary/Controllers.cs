72bee725813fd4c377eed56ed53cfb57f39dfa67
Jeshibu <jeshibu@gmail.com>
2022-12-24 17:09:00 +0100

    Failing attempt to focus Legacy Games Client window

--- source/LegacyGamesLibrary/Controllers.cs
+++ source/LegacyGamesLibrary/Controllers.cs
@@ -6,6 +6,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -101,8 +103,7 @@ namespace LegacyGamesLibrary
             if (!File.Exists(launcherPath))
                 throw new Exception("Can't launch Legacy Games Launcher. Installation not found.");
 
-            playniteAPI.Dialogs.ShowMessage("Use the Legacy Games Launcher to install the game. \nIt will be properly marked as installed in Playnite automatically.", "User action required");
-
+            FocusWindow();
             Process.Start(launcherPath);
 
             StartInstallWatcher();
@@ -144,5 +145,109 @@ namespace LegacyGamesLibrary
                 await Task.Delay(2000);
             }
         }
+
+        private void FocusWindow()
+        {
+            // Get Steam's process ID for comparison with child process parent IDs
+            var process = Process.GetProcessesByName("Legacy Games Launcher")?.FirstOrDefault();
+            if (process == null)
+            {
+                logger.Info("Couldn't focus window - Legacy Games Launcher isn't running");
+                return;
+            }
+
+            var windows = GetProcessWindows(process);
+
+            if (windows.Count == 0)
+            {
+                logger.Info("No windows found to focus.");
+                SetForegroundWindow(process.MainWindowHandle);
+                return;
+            }
+
+            foreach (var window in windows)
+            {
+                // Wait for the previous focus call to resolve
+                // Otherwise this one might be ignored
+                Thread.Sleep(10);
+                logger.Debug($"Setting foreground window: {window.Handle} {window.Title}");
+                SetForegroundWindow(window.Handle);
+            }
+        }
+
+        #region FindWindows
+
+        private class WindowInfo
+        {
+            public WindowInfo(IntPtr handle, string title)
+            {
+                Handle = handle;
+                Title = title;
+            }
+
+            public IntPtr Handle { get; set; }
+            public string Title { get; set; }
+        }
+
+        private static List<WindowInfo> GetProcessWindows(Process process)
+        {
+            var windows = new List<WindowInfo>();
+
+            EnumWindows(delegate (IntPtr hWnd, IntPtr lParam)
+            {
+                GetWindowThreadProcessId(hWnd, out uint processId);
+                if (process.Id != processId)
+                {
+                    // Return true here so that we iterate all windows
+                    return true;
+                }
+
+                var text = GetWindowText(hWnd);
+
+                windows.Add(new WindowInfo(hWnd, text));
+                return true;
+            }, IntPtr.Zero);
+
+            return windows;
+        }
+
+        /// <summary> Get the text for the window pointed to by hWnd </summary>
+        private static string GetWindowText(IntPtr hWnd)
+        {
+            int size = GetWindowTextLength(hWnd);
+            if (size > 0)
+            {
+                var builder = new StringBuilder(size + 1);
+                GetWindowText(hWnd, builder, builder.Capacity);
+                return builder.ToString();
+            }
+
+            return string.Empty;
+        }
+
+        #endregion FindWindows
+
+        #region PrivateImports
+
+        private delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool EnumWindows(EnumWindowsProc lpEnumFunc, IntPtr lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        #endregion
     }
 }
