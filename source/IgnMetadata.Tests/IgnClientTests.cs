5fd4c5c323e2eacf4c252b8790ed7159b4f73bec
Jeshibu <jeshibu@gmail.com>
2022-12-24 17:12:58 +0100

    IGN metadata test skeleton

---
+++ source/IgnMetadata.Tests/IgnClientTests.cs
@@ -0,0 +1,22 @@
+﻿using PlayniteExtensions.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IgnMetadata.Tests
+{
+    public class IgnClientTests
+    {
+        [Fact]
+        public void ActualWebRequest()
+        {
+            var downloader = new WebDownloader();
+            IgnClient client = new IgnClient(downloader);
+            var result = client.Search("fear");
+            Assert.NotEmpty(result);
+        }
+    }
+}
