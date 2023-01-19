b3ba41b01c3e4fef98b861c72984c2f8faf2ce10
Jeshibu <jeshibu@gmail.com>
2023-01-19 17:52:43 +0100

    Added Giant Bomb test project

---
+++ source/GiantBombMetadata.Tests/HtmlTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GiantBombMetadata.Tests
+{
+    public class HtmlTests
+    {
+        [Theory]
+        [InlineData("//")]
+        [InlineData("///")]
+        public void BunkPathTurnsIntoBaseUrl(string path)
+        {
+            string baseUrl = "https://www.giantbomb.com/soldier-of-fortune/3030-22547/";
+            string htmlFormat = "<a href=\"{0}\">link</a>";
+            string input = string.Format(htmlFormat, path);
+            string expected = string.Format(htmlFormat, baseUrl);
+
+            string processed = GiantBombHelper.MakeHtmlUrlsAbsolute(input, baseUrl);
+
+            Assert.Equal(expected, processed);
+        }
+    }
+}
