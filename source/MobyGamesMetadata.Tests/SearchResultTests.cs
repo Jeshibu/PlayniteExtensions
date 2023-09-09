1903c467641119a1630fdd309d3631e5489b0010
Jeshibu <jeshibu@gmail.com>
2023-09-09 17:48:47 +0200

    MobyGames: Added failing test for #43

---
+++ source/MobyGamesMetadata.Tests/SearchResultTests.cs
@@ -0,0 +1,16 @@
+﻿using MobyGamesMetadata.Api;
+using Xunit;
+
+namespace MobyGamesMetadata.Tests
+{
+    public class SearchResultTests
+    {
+        [Fact]
+        public void NumericTitleUrlParsesCorrectId()
+        {
+            var sr = new SearchResult();
+            sr.SetUrlAndId("https://www.mobygames.com/game/86550/640/");
+            Assert.Equal(86550, sr.Id);
+        }
+    }
+}
