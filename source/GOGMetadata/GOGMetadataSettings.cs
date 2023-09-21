3c15e7ac14f5768b2b0d06cf460c1b85448a21c9
Jeshibu <jeshibu@gmail.com>
2023-09-21 23:03:32 +0200

    GOG: added default language setting inheritance from Playnite

--- source/GOGMetadata/GOGMetadataSettings.cs
+++ source/GOGMetadata/GOGMetadataSettings.cs
@@ -24,7 +24,15 @@ namespace GOGMetadata
             else
             {
                 Settings = new GOGMetadataSettings();
+                SetMetadataLanguageByPlayniteLanguage();
             }
+        }
+
+        private void SetMetadataLanguageByPlayniteLanguage()
+        {
+            var langCode = PlayniteApi.ApplicationSettings.Language.Substring(0, 2);
+            if (Languages.ContainsKey(langCode))
+                Settings.Locale = langCode;
         }
 
         public Dictionary<string, string> Languages { get; } = new Dictionary<string, string>
