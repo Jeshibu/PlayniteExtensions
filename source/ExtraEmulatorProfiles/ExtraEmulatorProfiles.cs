9d2888b53f5caf823125b937fc45f9a7c070f4e9
Jeshibu <jeshibu@gmail.com>
2025-02-19 23:43:21 +0100

    Added Extra Emulation Profiles plugin

---
+++ source/ExtraEmulatorProfiles/ExtraEmulatorProfiles.cs
@@ -0,0 +1,49 @@
+﻿using Playnite.SDK;
+using Playnite.SDK.Events;
+using Playnite.SDK.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+
+namespace ExtraEmulatorProfiles
+{
+    public class ExtraEmulatorProfiles : GenericPlugin
+    {
+        private static readonly ILogger logger = LogManager.GetLogger();
+
+        private ExtraEmulatorProfilesSettingsViewModel settings { get; set; }
+
+        public override Guid Id { get; } = Guid.Parse("e4bf38d6-d2fd-4741-a269-47becfd55433");
+
+        public ExtraEmulatorProfiles(IPlayniteAPI api) : base(api)
+        {
+            settings = new ExtraEmulatorProfilesSettingsViewModel(this);
+            Properties = new GenericPluginProperties
+            {
+                HasSettings = true
+            };
+        }
+
+        public override void OnApplicationStarted(OnApplicationStartedEventArgs args)
+        {
+            if (settings.Settings.InstalledPatchVersion < settings.PluginVersion)
+                settings.ExecutePatch();
+        }
+
+        public override ISettings GetSettings(bool firstRunSettings)
+        {
+            return settings;
+        }
+
+        public override UserControl GetSettingsView(bool firstRunSettings)
+        {
+            return new ExtraEmulatorProfilesSettingsView();
+        }
+
+        public override IEnumerable<MainMenuItem> GetMainMenuItems(GetMainMenuItemsArgs args)
+        {
+            yield return new MainMenuItem { MenuSection = @"Extra Emulator Profiles", Description = "Add and update emulator profiles", Action = _ => settings.ExecutePatch() };
+            yield return new MainMenuItem { MenuSection = @"Extra Emulator Profiles", Description = "Reset emulator profiles to defaults", Action = _ => settings.Reset() };
+        }
+    }
+}
---
+++ source/ExtraEmulatorProfiles/ExtraEmulatorProfilesSettings.cs
@@ -0,0 +1,88 @@
+﻿using Playnite.SDK;
+using Playnite.SDK.Data;
+using PlayniteExtensions.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace ExtraEmulatorProfiles
+{
+    public class ExtraEmulatorProfilesSettings : ObservableObject
+    {
+        private Version installedPatchVersion = new Version(0, 0);
+
+        [DontSerialize]
+        public Version InstalledPatchVersion { get => installedPatchVersion; set => SetValue(ref installedPatchVersion, value); }
+
+        public string InstalledPatchVersionString
+        {
+            get => InstalledPatchVersion.ToString();
+            set => InstalledPatchVersion = new Version(value);
+        }
+    }
+
+    public class ExtraEmulatorProfilesSettingsViewModel : PluginSettingsViewModel<ExtraEmulatorProfilesSettings, ExtraEmulatorProfiles>
+    {
+        public Version PluginVersion { get; private set; }
+        public string PlayniteEmulationDirectory { get; private set; }
+        public string PatchDirectory { get; private set; }
+        public string OriginalsDirectory { get; private set; }
+
+        public ExtraEmulatorProfilesSettingsViewModel(ExtraEmulatorProfiles plugin) : base(plugin, plugin.PlayniteApi)
+        {
+            Settings = plugin.LoadPluginSettings<ExtraEmulatorProfilesSettings>() ?? new ExtraEmulatorProfilesSettings();
+
+            var assembly = Assembly.GetExecutingAssembly();
+
+            PluginVersion = assembly.GetName().Version;
+            PlayniteEmulationDirectory = $@"{PlayniteApi.Paths.ApplicationPath}\Emulation\";
+            var assemblyDir = new FileInfo(assembly.Location).DirectoryName;
+            PatchDirectory = $@"{assemblyDir}\EmulationFiles\Patch\";
+            OriginalsDirectory = $@"{assemblyDir}\EmulationFiles\Original\";
+        }
+
+        public void ExecutePatch() => CopyFiles(PatchDirectory, PluginVersion);
+
+        public void Reset()
+        {
+            DeleteFiles(PlayniteEmulationDirectory, "*.yaml");
+            DeleteFiles(PlayniteEmulationDirectory + "Emulators", "*", SearchOption.AllDirectories);
+
+            CopyFiles(OriginalsDirectory, new Version(0, 0));
+        }
+
+        public RelayCommand PatchCommand => new RelayCommand(ExecutePatch);
+        public RelayCommand ResetCommand => new RelayCommand(Reset);
+
+        private void CopyFiles(string baseDirectory, Version version)
+        {
+            var patchFiles = Directory.GetFiles(baseDirectory, "*", SearchOption.AllDirectories);
+            foreach (var file in patchFiles)
+            {
+                var relativePath = file.TrimStart(baseDirectory);
+                var targetPath = PlayniteEmulationDirectory + relativePath;
+                Directory.CreateDirectory(Path.GetDirectoryName(targetPath));
+                File.Copy(file, targetPath, overwrite: true);
+            }
+
+            Settings.InstalledPatchVersion = version;
+            Plugin.SavePluginSettings(Settings);
+
+            PlayniteApi.Dialogs.ShowMessage(
+                $"Copied {patchFiles.Length} files to the emulator profiles directory. Restart Playnite to apply these changes.",
+                "Extra Emulator Profiles",
+                System.Windows.MessageBoxButton.OK,
+                System.Windows.MessageBoxImage.Information);
+        }
+
+        private void DeleteFiles(string directory, string filter = "*", SearchOption searchOption = SearchOption.TopDirectoryOnly)
+        {
+            var files = Directory.GetFiles(directory, filter, searchOption);
+            foreach (var f in files)
+            {
+                File.Delete(f);
+            }
+        }
+    }
+}
