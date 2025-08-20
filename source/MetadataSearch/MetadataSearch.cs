5abfadce60503369c6cd31f8379d3991af2a7f06
Jeshibu <jeshibu@gmail.com>
2025-08-20 21:23:12 +0200

    Metadata Search: add labels to context

--- source/MetadataSearch/MetadataSearch.cs
+++ source/MetadataSearch/MetadataSearch.cs
@@ -13,20 +13,20 @@ public class MetadataSearch : GenericPlugin
     public MetadataSearch(IPlayniteAPI playniteAPI) : base(playniteAPI)
     {
         Searches = [
-            new("fp", "Filter presets", new DatabaseObjectFilterSearchContext<FilterPreset, FilterPresetSearchItem>(playniteAPI, pn=>pn.Database.FilterPresets, x=>new(playniteAPI.MainView, x))),
-            new("library", "Filter by library", new DatabaseObjectFilterSearchContext<DatabaseObject, LibraryFilterSearchItem>(playniteAPI, pn=>pn.Addons.Plugins.OfType<LibraryPlugin>().Select(p=>new DatabaseObject{Id=p.Id,Name=p.Name }), x=>new(playniteAPI.MainView, x))),
-            new("agerating", "Filter by age rating", new DatabaseObjectFilterSearchContext<AgeRating, AgeRatingFilterSearchItem>(playniteAPI, pn=>pn.Database.AgeRatings, x=>new(playniteAPI.MainView, x))),
-            new("category", "Filter by category", new DatabaseObjectFilterSearchContext<Category, CategoryFilterSearchItem>(playniteAPI, pn=>pn.Database.Categories, x=>new(playniteAPI.MainView, x))),
-            new("completion", "Filter by completion status", new DatabaseObjectFilterSearchContext<CompletionStatus, CompletionStatusFilterSearchItem>(playniteAPI, pn=>pn.Database.CompletionStatuses, x=>new(playniteAPI.MainView, x))),
-            new("developer", "Filter by developer", new DatabaseObjectFilterSearchContext<Company, DeveloperFilterSearchItem>(playniteAPI, pn=>pn.Database.Companies, x=>new(playniteAPI.MainView, x))),
-            new("publisher", "Filter by publisher", new DatabaseObjectFilterSearchContext<Company, PublisherFilterSearchItem>(playniteAPI, pn=>pn.Database.Companies, x=>new(playniteAPI.MainView, x))),
-            new("feature", "Filter by feature", new DatabaseObjectFilterSearchContext<GameFeature, FeatureFilterSearchItem>(playniteAPI, pn=>pn.Database.Features, x=>new(playniteAPI.MainView, x))),
-            new("genre", "Filter by genre", new DatabaseObjectFilterSearchContext<Genre, GenreFilterSearchItem>(playniteAPI, pn=>pn.Database.Genres, x=>new(playniteAPI.MainView, x))),
-            new("platform", "Filter by platform", new DatabaseObjectFilterSearchContext<Platform, PlatformFilterSearchItem>(playniteAPI, pn=>pn.Database.Platforms, x=>new(playniteAPI.MainView, x))),
-            new("region", "Filter by region", new DatabaseObjectFilterSearchContext<Region, RegionFilterSearchItem>(playniteAPI, pn=>pn.Database.Regions, x=>new(playniteAPI.MainView, x))),
-            new("series", "Filter by series", new DatabaseObjectFilterSearchContext<Series, SeriesFilterSearchItem>(playniteAPI, pn=>pn.Database.Series, x=>new(playniteAPI.MainView, x))),
-            new("source", "Filter by source", new DatabaseObjectFilterSearchContext<GameSource, SourceFilterSearchItem>(playniteAPI, pn=>pn.Database.Sources, x=>new(playniteAPI.MainView, x))),
-            new("tag", "Filter by tag", new DatabaseObjectFilterSearchContext<Tag,TagFilterSearchItem>(playniteAPI, pn=>pn.Database.Tags, x=>new(playniteAPI.MainView, x)))
+            new("fp", "Filter presets", new DatabaseObjectFilterSearchContext<FilterPreset, FilterPresetSearchItem>(playniteAPI, pn=>pn.Database.FilterPresets, x=>new(playniteAPI.MainView, x)){Label = "Filter presets"}),
+            new("library", "Filter by library", new DatabaseObjectFilterSearchContext<DatabaseObject, LibraryFilterSearchItem>(playniteAPI, pn=>pn.Addons.Plugins.OfType<LibraryPlugin>().Select(p=>new DatabaseObject{Id=p.Id,Name=p.Name }), x=>new(playniteAPI.MainView, x)){Label = "Libraries"}),
+            new("agerating", "Filter by age rating", new DatabaseObjectFilterSearchContext<AgeRating, AgeRatingFilterSearchItem>(playniteAPI, pn=>pn.Database.AgeRatings, x=>new(playniteAPI.MainView, x)){Label = "Age ratings"}),
+            new("category", "Filter by category", new DatabaseObjectFilterSearchContext<Category, CategoryFilterSearchItem>(playniteAPI, pn=>pn.Database.Categories, x=>new(playniteAPI.MainView, x)){Label = "Categories"}),
+            new("completion", "Filter by completion status", new DatabaseObjectFilterSearchContext<CompletionStatus, CompletionStatusFilterSearchItem>(playniteAPI, pn=>pn.Database.CompletionStatuses, x=>new(playniteAPI.MainView, x)){Label = "Completion statuses"}),
+            new("developer", "Filter by developer", new DatabaseObjectFilterSearchContext<Company, DeveloperFilterSearchItem>(playniteAPI, pn=>pn.Database.Companies, x=>new(playniteAPI.MainView, x)){Label = "Developers"}),
+            new("publisher", "Filter by publisher", new DatabaseObjectFilterSearchContext<Company, PublisherFilterSearchItem>(playniteAPI, pn=>pn.Database.Companies, x=>new(playniteAPI.MainView, x)){Label = "Publishers"}),
+            new("feature", "Filter by feature", new DatabaseObjectFilterSearchContext<GameFeature, FeatureFilterSearchItem>(playniteAPI, pn=>pn.Database.Features, x=>new(playniteAPI.MainView, x)){Label = "Features"}),
+            new("genre", "Filter by genre", new DatabaseObjectFilterSearchContext<Genre, GenreFilterSearchItem>(playniteAPI, pn=>pn.Database.Genres, x=>new(playniteAPI.MainView, x)){Label = "Genres"}),
+            new("platform", "Filter by platform", new DatabaseObjectFilterSearchContext<Platform, PlatformFilterSearchItem>(playniteAPI, pn=>pn.Database.Platforms, x=>new(playniteAPI.MainView, x)){Label = "Platforms"}),
+            new("region", "Filter by region", new DatabaseObjectFilterSearchContext<Region, RegionFilterSearchItem>(playniteAPI, pn=>pn.Database.Regions, x=>new(playniteAPI.MainView, x)){Label = "Regions"}),
+            new("series", "Filter by series", new DatabaseObjectFilterSearchContext<Series, SeriesFilterSearchItem>(playniteAPI, pn=>pn.Database.Series, x=>new(playniteAPI.MainView, x)){Label = "Series"}),
+            new("source", "Filter by source", new DatabaseObjectFilterSearchContext<GameSource, SourceFilterSearchItem>(playniteAPI, pn=>pn.Database.Sources, x=>new(playniteAPI.MainView, x)){Label = "Sources"}),
+            new("tag", "Filter by tag", new DatabaseObjectFilterSearchContext<Tag,TagFilterSearchItem>(playniteAPI, pn=>pn.Database.Tags, x=>new(playniteAPI.MainView, x)){Label = "Tags"})
         ];
     }
 
