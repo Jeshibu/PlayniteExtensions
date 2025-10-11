47f15926184340e7bc2aaf4bc4dd83a48d6d6551
Jeshibu <jeshibu@gmail.com>
2025-10-11 02:32:53 +0200

    EA: delete obsolete models

--- source/EaLibrary/Models/Obsolete/AuthTokenResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EaLibrary.Models;
-
-public class AuthTokenResponse
-{
-    public string error;
-    public string expires_in;
-    public string token_type;
-    public string access_token;
-}
--- source/EaLibrary/Models/Obsolete/OriginGameMetadata.cs
+++
@@ -1,9 +0,0 @@
-﻿using Playnite.SDK.Models;
-
-namespace EaLibrary.Models;
-
-public class OriginGameMetadata : GameMetadata
-{
-    public GameStoreDataResponse StoreDetails { get; set; }
-    public StorePageMetadata StoreMetadata { get; set; }
-}
